892c90477e2490e139845911925f982f9095b4ee
hunanyanmeri033 <hunanyanmeri033@gmail.com>
2026-06-20 19:01:11 -0700

    kind of finished parser

--- scripts.py
+++ scripts.py
@@ -71,49 +71,71 @@ SPICE netlist BNF
 
 <element> ::= "R" <name> <node1> <node2> <value>
 """
+"""
 #full netlist
 class netlist():
     def __init__(comment):
         if comment:
-            self.comment = comment
+            #self.comment = comment
+"""
 
-class comment(netlist):
+
+class comment():
     def __init__(self, comment):
         self.comment = comment
 
-class fund3():
+"""class fund3():
+    def __init__(self, name, node1, node2, value):
+        self.name = name
+        self.node1 = node1
+        self.node2 = node2
+        self.value = value"""
+
+class resistor():
+    def __init__(self, name, node1, node2, value):
+        self.name = name
+        self.node1 = node1
+        self.node2 = node2
+        self.value = value
+    def __str__(self):
+        return (f"Resistor named {self.name} connecting from port {self.node1} to port {self.node2} with a value of {self.value}")
+
+class capacitor():
     def __init__(self, name, node1, node2, value):
         self.name = name
         self.node1 = node1
         self.node2 = node2
         self.value = value
 
-class resistor(fund3):
+class inductor():
     def __init__(self, name, node1, node2, value):
-        super().__init__(name, node1, node2, value)
+        self.name = name
+        self.node1 = node1
+        self.node2 = node2
+        self.value = value
 
-class capacitor(fund3):
-    def __init__(self, name, node1, node2, value):
-        super().__init__(name, node1, node2, value)
+"""class Indep_source():
+    def __init__(self, name, pos, neg, source_spec):
+        self.name = name
+        self.pos = pos
+        self.neg = neg
+        self.spec = source_spec"""
 
-class inductor(fund3):
-    def __init__(self, name, node1, node2, value):
-        super().__init__(name, node1, node2, value)
-
-class Indep_source():
+class voltage_source():
     def __init__(self, name, pos, neg, source_spec):
         self.name = name
         self.pos = pos
         self.neg = neg
         self.spec = source_spec
+    def __str__(self):
+        return f"Independent voltage source named {self.name} connecting {self.pos} to {self.neg} with these source spec: {self.spec}"
 
-class voltage_source(Indep_source):
+class current_source():
     def __init__(self, name, pos, neg, source_spec):
-        super().__init__(name, pos, neg, source_spec)
-
-class current_source(Indep_source):
-    def __init__(self, name, pos, neg, source_spec):
-        super().__init__(name, pos, neg, source_spec)
+        self.name = name
+        self.pos = pos
+        self.neg = neg
+        self.spec = source_spec
 
 class D():
     def __init__(self, name, node1, node2, model_name): # "D" <name> <node1> <node2> <model_name>
@@ -133,11 +155,13 @@ class Q():
 class M():
     def __init__(self, name, d, g, s, b, model_name): # "M" <name> <d> <g> <s> <b> <model_name>
         self.name = name
-        self.d=d
+        self.d = d
         self.g = g
         self.s = s
         self.b = b
         self.model_name = model_name
+    def __str__(self):
+        return f"M{self.name} {self.d} {self.g} {self.s} {self.b} {self.model_name}"
 
 class model():
     def __init__(self, name, type, param_list):
@@ -147,13 +171,16 @@ class model():
         
 #graph
 class node:
-    def __init__(self, source, target):
-        self.node = source
-        self.connect = []
-        self.connect.append(target)
+    def __init__(self, name):
+        self.name = name
+        self.edge = []
+    def add_edge(self, edge):
+        self.edge.append(edge)
 
-    def connect(self, target):
-        self.connect.append(target)
+class edge:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
 
 class subckt():
     def __init__(self, name, node_list, statement_list):
@@ -162,14 +189,23 @@ class subckt():
         self.statement_list = statement_list
         self.inst = []
     def instance(self, name, nodes, params = []):
-        self.inst.append([name], )
+        self.inst.append(name, nodes, params)
+
+class instance(subckt):
+    def __init__(self, name, nodes, subcircuit):
+        self.name = name
+        self.nodes = nodes
+        self.subcircuit = subcircuit
+
+
 print(type(lines))
 i = 0
 subcircuits = []
-
+dictnodes = {}
 def parser(lines):
     i = 0
     parsed_lines = []
+    dictnodes = {}
     while i != len(lines):
         if not lines[i]:
             print('empty')
@@ -184,30 +220,77 @@ def parser(lines):
                 lele = lines[i].split()
                 parsed_lines.append(resistor(lele[0][1:len(lele[0])-1], lele[1], lele[2], lele[3]))
                 print('Resistor')
+                if lele[1] in dictnodes:
+                    dictnodes[lele[1]].append(lele[2])
+                else:
+                    dictnodes[lele[1]] = [lele[2]]
+                if lele[2] in dictnodes:
+                    dictnodes[lele[2]].append(lele[1])
+                else:
+                    dictnodes[lele[2]] = [lele[1]]
                 i = i+1
             case 'C':
                 lele = lines[i].split()
                 parsed_lines.append(capacitor(lele[0][1:len(lele[0])-1], lele[1], lele[2], lele[3]))
-                print('Capacitor')
+                if lele[1] in dictnodes:
+                    dictnodes[lele[1]].append(lele[2])
+                else:
+                    dictnodes[lele[1]] = [lele[2]]
+                if lele[2] in dictnodes:
+                    dictnodes[lele[2].append(lele[1])]
+                else:
+                    dictnodes[lele[2]] = [lele[1]]
                 i = i+1
             case 'L':
                 lele = lines[i].split()
                 parsed_lines.append(inductor(lele[0][1:len(lele[0])-1], lele[1], lele[2], lele[3]))
                 print('inductor')
+                if lele[1] in dictnodes:
+                    dictnodes[lele[1]].append(lele[2])
+                else:
+                    dictnodes[lele[1]] = [lele[2]]
+                if lele[2] in dictnodes:
+                    dictnodes[lele[2]].append(lele[1])
+                else:
+                    dictnodes[lele[2]] = [lele[1]]
                 i = i+1
             case 'V':
                 lele = lines[i].split()
                 parsed_lines.append(voltage_source(lele[0][1:len(lele[0])-1], lele[1], lele[2], lele[3:]))
                 print("Independednt voltage source")
+                if lele[1] in dictnodes:
+                    dictnodes[lele[1]].append(lele[2])
+                else:
+                    dictnodes[lele[1]] = [lele[2]]
+                if lele[2] in dictnodes:
+                    dictnodes[lele[2]].append(lele[1])
+                else:
+                    dictnodes[lele[2]] = [lele[1]]
                 i = i+1
             case 'I':
                 lele = lines[i].split()
                 parsed_lines.append(current_source(lele[0][1:len(lele[0])-1], lele[1], lele[2], lele[3:]))
                 print("independent current source")
+                if lele[1] in dictnodes:
+                    dictnodes[lele[1]].append(lele[2])
+                else:
+                    dictnodes[lele[1]] = [lele[2]]
+                if lele[2] in dictnodes:
+                    dictnodes[lele[2]].append(lele[1])
+                else:
+                    dictnodes[lele[2]] = [lele[1]]
                 i = i+1
             case 'D': # "D" <name> <node1> <node2> <model_name>
                 lele = lines[i].split()
                 parsed_lines.append(D(lele[0][1:len(lele[0])-1], lele[1], lele[2], [3]))
+                if lele[1] in dictnodes:
+                    dictnodes[lele[1]].append(lele[2])
+                else:
+                    dictnodes[lele[1]] = [lele[2]]
+                if lele[2] in dictnodes:
+                    dictnodes[lele[2].append(lele[1])]
+                else:
+                    dictnodes[lele[2]] = [lele[1]]
                 print("D")
                 i = i+1
             case 'Q': # "Q" <name> <c> <b> <e> <model_name>
@@ -230,8 +313,8 @@ def parser(lines):
                     while lines[end_i][0] != ".":
                         end_i = end_i+1
                     print(lines[start_i:end_i])
-                    parsed_lines.append(subckt(lele[1], [lele[2:]], parser(lines[start_i:end_i])))
-                    subcircuits.append(subckt(lele[1], [lele[2:]], parser(lines[start_i:end_i])))
+                    parsed_lines.append(subckt(lele[1], [lele[2:]], parser(lines[start_i:end_i])[0]))
+                    subcircuits.append(subckt(lele[1], [lele[2:]], parser(lines[start_i:end_i])[0]))
                     print(end_i)
                     i=end_i+1
                     print("end")
@@ -281,7 +364,7 @@ def parser(lines):
                 """
                 for element in parsed_lines:
                     if type(element).__name__ == "subckt" and element.name == subcircuit_name:
-                        element.instance(inst, nodes, subcircuit_name)
+                        instance(inst, nodes,element)
                 i = i+1
                 print("instance")
 
@@ -289,15 +372,36 @@ def parser(lines):
 
                             
     print("hello")
-    return parsed_lines
-print(parser(lines))
-"""
-for element in parser(lines):
+    return parsed_lines, dictnodes
+
+
+results = parser(lines)
+dictnodes = results[1]
+parsed_list = results[0]
+print(dictnodes)
+objectdict = {}
+for key in dictnodes:
+    objectdict[key] = node(key)
+
+edges = []
+for key in objectdict:
+    for i in range(len(dictnodes[key])):
+        objectdict[key].add_edge(edge(key, dictnodes[key][i]))
+
+"""for element in parser(lines):
     
-    if type(element).__name__ == 'resistor':
-        node(element.node1, element.node2) 
-    elif type(element).__name__== 'capacitor':
-        node(element.node1, element.node2)
-    elif type(element).__name__ == 'inductor':
-        node(element.node1, element.node2)
-    """
+    match type(element).__name__:
+        case 'resistor':
+            
+            node.add_edge(edge(node(element.node1), node(element.node2)))
+        case 'capacitor':
+            node(element.node1, element.node2)
+        case 'inductor':
+            node(element.node1, element.node2)
+        case 'voltage_source':
+            node(element.pos, element.neg)
+        case 'current_source':
+            node(element.pos, element.neg)
+        case 
+
+"""
